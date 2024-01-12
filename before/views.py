087bf168bd48d5d0f9fdf802500d296462150e6e
unknown <rajputmeetali249@gmail.com>
2024-01-12 15:12:27 +0530

    second commit

--- before/views.py
+++ before/views.py
@@ -40,7 +40,4 @@ def update(request,id):
         return redirect('/show')
     return render(request,'edit.html',{'employee':employee})
 
-def destroy(request,id):
-    employee=Employee.objects.get(id=id)
-    employee.delete()
-    return redirect("/show")
+
